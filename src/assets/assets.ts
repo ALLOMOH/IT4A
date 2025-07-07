3ab7b4c685d8666582f6d9a8fe5c46dfc58a81b6
ALLOMOH <allomohangealex@gmail.com>
2025-07-07 00:16:08 +0000

    add base build of the website

---
+++ src/assets/assets.ts
@@ -0,0 +1,25 @@
+
+import Logo from "/logo.png";
+import Modif from "/image/bg/Modif.png";
+import LogoBlueLogo from "/image/logo/BlueLogo.png";
+import LogoBlueLogoH from "/image/logo/BlueLogoH.png";
+import LogoWhiteLogo from "/image/logo/WhiteLogo.png";
+import LogoWhiteLogoH from "/image/logo/WhiteLogoH.png";
+
+import Background from "/image/bg/backgroundMaquette.png";
+
+
+const AssetsImage :object = {
+    Logo,
+    LogoBlueLogo,
+    LogoBlueLogoH,
+    LogoWhiteLogo,
+    LogoWhiteLogoH,
+    Modif,
+    Background,
+}
+
+
+export default AssetsImage;
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    content:['./src/**/*.{astro,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      keyframes: {
+        shine: {
+          '0%': { 'background-position': '100%' },
+          '100%': { 'background-position': '-100%' },
+        },
+      },
+      animation: {
+        shine: 'shine 5s linear infinite',
+      },
+    },
+  },
+  plugins: [],
+};
