d24db720e0764dcbefe6971f204d91873b17df86
ALLOMOH <allomohangealex@gmail.com>
2025-08-06 12:27:53 +0000

    process 98.6% edited astroconfig install node

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
 import node from '@astrojs/node';
 // https://astro.build/config
-export default defineConfig({
+export default defineConfig({ // Ajoutez cette ligne
   output:'server',
   integrations: [react()],
 
